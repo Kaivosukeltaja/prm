0fb12dcade6e655785e79a26d21b2f0539e0c005
Niko Salminen <niko@salminen.me>
2019-05-28 22:15:05 +0300

    Add basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,108 @@
 import React from "react"
-// import { Helmet } from "react-helmet"
+import { Container, Row, Col } from 'reactstrap'
 import { graphql } from 'gatsby'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../static/css/styles.css'
+
+const Page = styled.div`
+  background-color: #000;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  color: #ffffff; 
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 80em;
+  background-color: #333;
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  padding: 1em;
+  align-items: center;
+`;
+
+const NavBarBg = styled.div`
+  background-color: #B4EB45;
+  background-image: linear-gradient(to right, #B4EB45, #7ED321);
+`;
+
+const MainContent = styled.div`
+  padding: 2rem 1rem;
+`;
+
+const MainContentBg = styled.div`
+  background-color: #333333;
+`;
+
+const Sidebar = styled.aside`
+  background-color: #333333;
+  padding: 2rem 1rem;
+`;
+
+const EpisodeList = styled.div`
+  border: 1px solid #ffffff;
+  padding: 1em;
+`;
+
+const Column = styled.div`
+  display: flex;
+`;
 
 export default ({ data }) => {
   const episodes = data.episodes.edges;
   const page = data.page;
   return (
-  <div>
-    { /*
-    <Helmet>
-      <title>{ data.site.siteMetadata.title }</title>
-      <link rel="canonical" href={ data.site.siteMetadata.siteUrl } />
-      <meta charSet="utf-8" />
-      <meta name="description" content={ data.site.siteMetadata.description } />
-    </Helmet> */ }
-    { episodes.map(({ node: episode }) => (
-      <div key={episode.id}>
-        <div dangerouslySetInnerHTML={{ __html: page.edges[0].node.html }} />
-        <Link to={episode.frontmatter.episodeNumber}>
-          {episode.frontmatter.title}
-        </Link>
-      </div>
-    ))}
-  </div>
-)}
+    <Page>
+      { /*
+      <Helmet>
+        <title>{ data.site.siteMetadata.title }</title>
+        <link rel="canonical" href={ data.site.siteMetadata.siteUrl } />
+        <meta charSet="utf-8" />
+        <meta name="description" content={ data.site.siteMetadata.description } />
+      </Helmet> */ }
+      <Container>
+        <NavBarBg>
+          <Row>
+            <Col>
+              <NavBar>
+                <p>Parin Rivin Muutos</p>
+              </NavBar>
+            </Col>
+          </Row>
+        </NavBarBg>
+        <MainContentBg>
+          <Row>
+            <Col sm="8">
+              <MainContent>
+                <div dangerouslySetInnerHTML={{ __html: page.edges[0].node.html }} />
+                <EpisodeList>
+                  { episodes.map(({ node: episode }) => (
+                    <div key={episode.id}>
+                      <Link to={"/" + episode.frontmatter.episodeNumber}>
+                        {episode.frontmatter.title}
+                      </Link>
+                    </div>
+                  ))}
+                </EpisodeList>
+              </MainContent>
+            </Col>
+            <Col sm="4">
+              <Sidebar>
+                <div dangerouslySetInnerHTML={{ __html: page.edges[1].node.html }} />
+              </Sidebar>
+            </Col>
+          </Row>
+        </MainContentBg>
+      </Container>
+    </Page>
+  );
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -61,16 +139,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
-/*
-export const MainPageQuery = graphql`
-  query MainPageQuery {
-    site {
-      siteMetadata {
-        title
-        siteUrl
-        description        
-      }
-    }
-  }
-`*/
+`;
---
+++ src/pages/index/sidebar.md
@@ -0,0 +1,17 @@
+---
+title: Sivupalkki
+templateKey: index-sidebar
+---
+#Houstit
+
+**Niko Salminen**
+
+Kaikesta uudesta ja kiiltävästä innostunut koodijäärä, jonka ura on kulkenut 90-luvun multimediaromppudevaajasta Javan ja PHP:n kautta fronttidevauksen ja ketterän kehityksen ihmeelliseen maailmaan. Pyörittää kotonakin scrumia ja treenikämpällä kanbania.
+
+**Pasi Honkanen**
+
+Prosessi Pasinakin tunnettu menetelmämies, joka on kulkenut melkein joka sopen softakehityskentässä. Organisaatiot ja erityisesti digitaaliset palveluorganisaatiot ovat tämän päivän kasvusuunta, mutta vannoutuneelta TDD mieheltä devauskin taipuu vielä pöytälaatikkotasolla Reactista Scalaan ja ehkä sieltä vielä jostain syövereistä Swingikin irtoaa. Ketteryyttä on treenattu jo vuosituhannen alusta asti.
+
+**Antti "Rintsi" Rintala**
+
+Omien silmiensä takaa softakehitystä ymmärtävä arkkitehdin planttu, joka myös koodailee edelleen, kun sille päälle sattuu. Tämän hetken kiinnostuksen kohteina ovat erityisesti yritysten auttaminen isojen kokonaisuuksien hahmottamisessa ja toteuttamisessa. Azure ja IoT ovat viime aikojen uusia aluevaltauksia.
